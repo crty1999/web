dce620dff365271fa21137e55bb29f697e93fb4a
crty1999 <45713906+crty1999@users.noreply.github.com>
2021-01-30 21:36:02 +0700

    Delete labs/lab3 directory

--- labs/lab3/fitness/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CustomUser)
-admin.site.register(Coach)
-admin.site.register(Client)
-admin.site.register(LessonSession)
-admin.site.register(Booking)
-admin.site.register(LessonType)
--- labs/lab3/fitness/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FitnessConfig(AppConfig):
-    name = 'fitness'
